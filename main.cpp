d3f6a8080ea868d42f3d1dae1e6b260313d05181
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 09:50:09 +0100

    Checks for directory

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 
 std::string get_template_file(const int number_of_arguments, char *arguments[]) {
@@ -17,8 +18,26 @@ std::string get_template_file(const int number_of_arguments, char *arguments[]) 
     return target.substr(2);
 }
 
+void generate_template(const std::string template_file) {
+    std::ifstream file(template_file);
+
+    if (!file.is_open()) throw std::runtime_error("Could not open file");
+
+    int spaces = 0;
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.length() < spaces + 1) throw std::runtime_error("Invalid template template");
+
+        bool is_directory = line[spaces] == '/';
+        if (is_directory) {
+        } else {
+        }
+    }
+    file.close();
+}
+
 int main(const int argc, char *argv[]) {
     const auto template_file = get_template_file(argc, argv);
-
+    generate_template(template_file);
     return 0;
 }
