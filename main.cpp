ab782e844837629cc7e4be821a760b5f560a811b
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-09 09:41:16 +0100

    Gets template file name

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
-int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+std::string get_template_file(const int number_of_arguments, char *arguments[]) {
+    if (number_of_arguments < 2) {
+        throw std::runtime_error("Not enough arguments");
     }
+
+    const std::string target = arguments[1];
+    if (target.length() < 3) {
+        throw std::runtime_error("Invalid target template");
+    }
+
+    for (int i = 0; i < 2; i++) {
+        if (target[i] != '-') throw std::runtime_error("Invalid target template");
+    }
+
+    return target.substr(2);
+}
+
+int main(const int argc, char *argv[]) {
+    const auto template_file = get_template_file(argc, argv);
 
     return 0;
 }
